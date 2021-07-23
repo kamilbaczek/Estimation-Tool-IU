bd5116dcf005b3ce42482d3c0600d439eb9ec689
Kamil Bączek <kaba@software.com>
2021-07-23 17:15:44 +0200

    [Http][BaseUrl] - read base url from configuration

--- src/helpers/http-comunicator.js
+++ src/helpers/http-comunicator.js
@@ -1,16 +1,13 @@
 import axios from "axios";
-import {authHeader} from './authservice/auth-header';
+import { authHeader } from "./authservice/auth-header";
 
 export const anonymousHttpClient = axios.create({
-  baseURL: 'https://localhost:5001/api/',
-  timeout: 1000,
+  baseURL: process.env.VUE_APP_BASE_URL,
 });
 
-
 const authenticatedHttpClient = axios.create({
-  baseURL: 'https://localhost:5001/api/',
-  timeout: 1000,
-  headers: authHeader()
+  baseURL: process.env.VUE_APP_BASE_URL,
+  headers: authHeader(),
 });
 
 export default authenticatedHttpClient;
