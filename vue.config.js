7f405162c907303f37bf3ff7e6d09265addb2a3b
Kamil Bączek <74410956+kamilbaczek@users.noreply.github.com>
2022-04-09 14:32:19 +0200

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,12 +1,4 @@
 module.exports = {
-  pluginOptions: {
-    i18n: {
-      locale: "en",
-      fallbackLocale: "en",
-      localeDir: "locales",
-      enableInSFC: true
-    },
-  },
   configureWebpack: {
     devtool: "source-map",
   },
